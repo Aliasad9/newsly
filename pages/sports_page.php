de7f0f4abd263aa84e67c37e59352532d3035778
Aliasad9 <aliasad6521@gmail.com>
2021-01-13 18:47:43 +0500

    made header modular

--- pages/sports_page.php
+++ pages/sports_page.php
@@ -1,36 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="description" content="Bootstrap News Website Theme" />
-    <title>Sports</title>
-
-    <!-- Bootstrap core CSS -->
-    <link
-      href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
-      rel="stylesheet"
-      integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1"
-      crossorigin="anonymous"
-    />
-
-    <meta name="theme-color" content="#7952b3" />
-
-    <!-- Custom styles for this template -->
-    <link
-      href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap"
-      rel="stylesheet"
-    />
-    <!-- Custom styles for this template -->
-    <link href="../styles/style.css" rel="stylesheet" />
-  </head>
+<?php
+$title="Sports";
+include_once('../inc/header.php');
+?>
   <body>
     <div class="container">
       <!-- Header -->
       <header class="news-header py-4">
         <div class="row flex-nowrap justify-content-center align-items-center">
           <div class="col-4 text-center">
-            <a class="news-header-logo text-dark" href="../index.html"
+            <a class="news-header-logo text-dark" href="../index.php"
               >Newsly</a
             >
           </div>
