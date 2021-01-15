4464e57f7e15d65491e63d55df687f8938d7a557
Aliasad9 <aliasad6521@gmail.com>
2021-01-15 21:11:15 +0500

    Featured Card made dynamic on each Category Page

--- inc/featured_card.php
+++ inc/featured_card.php
@@ -1,10 +1,10 @@
-<!-- Big Featured Card -->
+
 <div
   class="mb-4 text-white rounded-3 bg-dark main-sports-card"
 >
   <div class="wrapper rounded-3 p-4 p-md-5 d-flex align-items-end">
     <div class="col-md-6 px-0">
-      <h1 class="display-4 font-italic">Sports</h1>
+      <h1 class="display-4 font-italic"><?php echo $title;?></h1>
       <p class="lead my-3">
         Find the latest in top matches, highlights and expert analysis!
       </p>
