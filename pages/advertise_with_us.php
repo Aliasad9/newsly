3fb99c21d18d528f777f877f33aa02aba3bbb4a0
codewithfahad <codewithfahad@gmail.com>
2021-01-16 22:32:34 +0500

    Add advertise with us page layout

---
+++ pages/advertise_with_us.php
@@ -0,0 +1,178 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = "Advertise With Us";
+include_once('../inc/header.php');
+?>
+
+<body>
+
+    <!-- Header -->
+    <header class="news-header py-4">
+        <div class="row flex-nowrap justify-content-center align-items-center">
+            <div class="col-4 text-center">
+                <a class="news-header-logo text-dark" href="/newsly/index.php">Newsly</a>
+            </div>
+        </div>
+    </header>
+
+    <div class="container">
+
+        <!-- Advertising on our site -->
+        <div class="section-heading d-flex justify-content-between align-items-center py-4">
+            <h3 class="font-italic">Advertising On Our Site</h3>
+        </div>
+
+        <div class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum alias ducimus laboriosam ullam asperiores nobis optio enim nihil porro tempore, dicta ipsam provident exercitationem veniam nemo eaque repellendus excepturi cum eveniet animi quod quaerat iste. Numquam eum modi deserunt rem.
+            </p>
+        </div>
+
+        <!-- Examples -->
+        <div class="row">
+
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <img src="../assets/contact-image-4.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <strong class="d-inline-block mb-2 text-success">Design</strong>
+                        <h5 class="card-title">Card title</h5>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Nostrum laboriosam
+                            consectetur distinctio qui nulla sint iure
+                            corrupti. Libero, architecto eveniet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <img src="../assets/contact-image-8.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <strong class="d-inline-block mb-2 text-warning">Design</strong>
+                        <h5 class="card-title">Card title</h5>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Nostrum laboriosam
+                            consectetur distinctio qui nulla sint iure
+                            corrupti. Libero, architecto eveniet.
+                        </p>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <img src="../assets/contact-image-6.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <strong class="d-inline-block mb-2 text-primary">Design</strong>
+                        <h5 class="card-title">Card title</h5>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Nostrum laboriosam
+                            consectetur distinctio qui nulla sint iure
+                            corrupti. Libero, architecto eveniet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="card mb-4">
+                    <img src="../assets/contact-image-7.jpg" class="card-img-top" alt="..." />
+                    <div class="card-body">
+                        <strong class="d-inline-block mb-2 text-primary">Design</strong>
+                        <h5 class="card-title">Card title</h5>
+                        <div class="mb-1 text-muted">Nov 11</div>
+                        <p class="card-text">
+                            Lorem ipsum dolor, sit amet consectetur
+                            adipisicing elit. Nostrum laboriosam
+                            consectetur distinctio qui nulla sint iure
+                            corrupti. Libero, architecto eveniet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!-- Benefits -->
+        <div class="section-heading d-flex justify-content-between align-items-center py-4">
+            <h3 class="font-italic">Benefits</h3>
+        </div>
+
+        <div class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum alias ducimus laboriosam ullam asperiores nobis optio enim nihil porro tempore, dicta ipsam provident exercitationem veniam nemo eaque repellendus excepturi cum eveniet animi quod quaerat iste. Numquam eum modi deserunt rem.
+            </p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, quasi ad. Iure vitae consequuntur facere? Sed corrupti aspernatur architecto aperiam iusto minus rerum ea cumque consequuntur atque accusantium tempora voluptatibus iste, in facere neque totam quam quaerat eos et ut!</p>
+        </div>
+
+        <!-- How to reach out -->
+        <div class="section-heading d-flex justify-content-between align-items-center py-4">
+            <h3 class="font-italic">How To Reach Out?</h3>
+        </div>
+
+        <!-- Contact Form -->
+        <div class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum alias ducimus laboriosam ullam asperiores nobis optio enim nihil porro tempore, dicta ipsam provident exercitationem veniam nemo eaque repellendus excepturi cum eveniet animi quod quaerat iste. Numquam eum modi deserunt rem.
+            </p>
+        </div>
+
+        <div class="advertise-background rounded-3 mb-5">
+            <div class="advertise-wrapper p-4 rounded-3">
+                <div class="card my-5 px-md-3 px-2" style="max-width: 32rem; margin: auto;">
+                    <div class="card-body">
+                        <p class="card-title display-6 pt-3">
+                            <i class="far fa-paper-plane"></i><span class="px-3">Get in Touch</span>
+                        </p>
+                        <form method="POST" class="py-3">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="name" placeholder="name" />
+                                <label for="name" class="form-label">Name</label>
+                                <div class="form-text">
+                                    Enter your name.
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="email" placeholder="email" />
+                                <label for="email" class="form-label">Email</label>
+                                <div class="form-text">
+                                    Enter your email address.
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="product-name" placeholder="product name" />
+                                <label for="product-name" class="form-label">Name</label>
+                                <div class="form-text">
+                                    Enter the product name.
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" placeholder="Enter your product description" id="description" style="height: 100px"></textarea>
+                                <label for="description">Description</label>
+                                <div class="form-text">
+                                    Enter the product description.
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                Send
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+    </div>
+
+    <?php include_once('../inc/footer.html') ?>
+</body>
+
+</html>
