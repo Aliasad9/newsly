3f50fd2e67084b84099533951330db99e45d2706
codewithfahad <codewithfahad@gmail.com>
2021-01-16 22:10:21 +0500

    Update contact us page layout

--- pages/contact_us.php
+++ pages/contact_us.php
@@ -11,15 +11,61 @@ include_once('../inc/header.php');
         include_once('../inc/navbar.html')
         ?>
 
-        <!-- TODO: Dynamically add background image -->
-        <?php
-        include_once('../inc/featured_card.html')
-        ?>
+        <!-- Contact Form -->
+        <div class="section-heading d-flex justify-content-between align-items-center py-4">
+            <h3 class="font-italic">Reach Out to Us!</h3>
+        </div>
+
+        <div class="mb-4">
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum alias ducimus laboriosam ullam asperiores nobis optio enim nihil porro tempore, dicta ipsam provident exercitationem veniam nemo eaque repellendus excepturi cum eveniet animi quod quaerat iste. Numquam eum modi deserunt rem.
+            </p>
+        </div>
+
+        <div class="form-background rounded-3 mb-5">
+            <div class="form-wrapper p-4 rounded-3">
+                <div class="card my-5 px-md-3 px-2" style="max-width: 32rem; margin: auto;">
+                    <div class="card-body">
+                        <p class="card-title display-6 pt-3">
+                            <i class="far fa-envelope-open"></i><span class="px-3">Contact</span>
+                        </p>
+                        <form method="POST" class="py-3">
+                            <div class="form-floating mb-3">
+                                <input type="text" class="form-control" id="subject" placeholder="subject" />
+                                <label for="subject" class="form-label">Subject</label>
+                                <div class="form-text">
+                                    Enter the subject.
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="email" placeholder="email" />
+                                <label for="email" class="form-label">Email</label>
+                                <div class="form-text">
+                                    Enter your email address.
+                                </div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" placeholder="Leave your message here" id="message" style="height: 100px"></textarea>
+                                <label for="message">Message</label>
+                                <div class="form-text">
+                                    Enter your message here.
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary">
+                                Send
+                            </button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
 
         <!-- Our Partners -->
         <div class="row mb-2">
             <div class="section-heading d-flex justify-content-between align-items-center py-4">
-                <h3 class="font-italic">Our Advertising Partners</h3>
+                <h3 class="font-italic">Our Clients</h3>
             </div>
 
             <div class="col-md-6">
@@ -89,50 +135,6 @@ include_once('../inc/header.php');
             </div>
         </div>
 
-        <!-- Contact Form -->
-        <div class="section-heading d-flex justify-content-between align-items-center py-4">
-            <h3 class="font-italic">Reach Out to Us!</h3>
-        </div>
-
-        <div class="form-background rounded-3 mb-5">
-            <div class="form-wrapper p-4 rounded-3">
-                <div class="card my-5 px-md-3 px-2" style="max-width: 32rem; margin: auto;">
-                    <div class="card-body">
-                        <p class="card-title display-6 pt-3">
-                            <i class="far fa-envelope-open"></i><span class="px-3">Contact</span>
-                        </p>
-                        <form method="POST" class="py-3">
-                            <div class="form-floating mb-3">
-                                <input type="text" class="form-control" id="subject" placeholder="subject" />
-                                <label for="subject" class="form-label">Subject</label>
-                                <div class="form-text">
-                                    Enter the subject.
-                                </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <input type="email" class="form-control" id="email" placeholder="email" />
-                                <label for="email" class="form-label">Email</label>
-                                <div class="form-text">
-                                    Enter your email address.
-                                </div>
-                            </div>
-                            <div class="form-floating mb-3">
-                                <textarea class="form-control" placeholder="Leave your message here" id="message" style="height: 100px"></textarea>
-                                <label for="message">Message</label>
-                                <div class="form-text">
-                                    Enter your message here.
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-primary">
-                                Send
-                            </button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
     </div>
 
     <?php include_once('../inc/footer.html') ?>
