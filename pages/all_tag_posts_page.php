69cd57d22bc923d4715002a739b941ccc7c21b75
codewithfahad <codewithfahad@gmail.com>
2021-01-17 21:31:05 +0500

    Complete layout for all posts by tag page

---
+++ pages/all_tag_posts_page.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php
+$title = "All Tag Posts";
+include_once('../inc/header.php');
+?>
+
+<body>
+    <div class="container">
+
+        <?php
+        include_once('../inc/navbar.html')
+        ?>
+
+        <main class="container">
+            <div class="heading my-5">
+                <p class="display-6">Tag:<span class="mx-2 badge rounded-pill bg-primary"><a class="text-white text-decoration-none" href="../pages/politics_page.php">Politics</a></span></p>
+            </div>
+
+            <!-- All Stories Section -->
+            <div class="row mb-2">
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-2.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-success">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-3.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-warning">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-5.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-primary">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-2.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-success">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-3.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-warning">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-5.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-primary">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-2">
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-2.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-success">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-3.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-warning">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card mb-4">
+                        <img src="../assets/politics-image-5.jpg" class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <strong class="d-inline-block mb-2 text-primary">Design</strong>
+                            <h5 class="card-title">Card title</h5>
+                            <div class="mb-1 text-muted">Nov 11</div>
+                            <p class="card-text">
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                                Nostrum laboriosam consectetur distinctio qui nulla sint iure
+                                corrupti. Libero, architecto eveniet.
+                            </p>
+                        </div>
+
+                        <div class="card-body">
+                            <a href="#" class="card-link">Read More</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+
+    <?php include_once('../inc/footer.html') ?>
+
+</body>
+
+</html>
